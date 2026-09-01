002be63e051fd33e391e8ab747c8101c663396de
flaviusi-bit <flaviusi@gmail.com>
2026-09-01 13:56:26 +0300

    fix: serve approved MISHA logo locally

--- src/LogoMark.jsx
+++ src/LogoMark.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-
-const APPROVED_LOGO_URL = 'https://raw.githubusercontent.com/flaviusi-bit/Misha.systems/f2870790cc626400ac0bb46981749161fcc8431e/src/misha-logo.svg';
+import approvedLogo from './misha-logo.svg';
 
 export default function LogoMark(){
   return (
     <img
       className="misha-logo-mark"
-      src={APPROVED_LOGO_URL}
+      src={approvedLogo}
       alt="MISHA Systems"
       draggable="false"
     />
